ab22a0a26e08b744c6101e2adfc08b1fb767a4d5
Vladimir <shitov@biocad.ru>
2021-03-08 23:49:14 +0700

    Add test_primers_generator_with_wrong_number()

--- prigen/test_prigen.py
+++ prigen/test_prigen.py
@@ -9,3 +9,11 @@ def test_primers_generator_with_wrong_length():
 
     with pytest.raises(ValueError):
         PrimersGenerator(length=0, gc_percentage=.5)
+
+
+def test_primers_generator_with_wrong_number():
+    with pytest.raises(ValueError):
+        PrimersGenerator(length=1, gc_percentage=.5, number_of_primers=0)
+
+    with pytest.raises(ValueError):
+        PrimersGenerator(length=1, gc_percentage=.5, number_of_primers=-999)
