e454f7d6a2a0fe99cc473bfe72182bcbc9e1a745
Vladimir <shitov@biocad.ru>
2021-03-08 23:47:15 +0700

    Add test with 0 length

--- prigen/test_prigen.py
+++ prigen/test_prigen.py
@@ -6,3 +6,6 @@ from prigen.generators import PrimersGenerator
 def test_primers_generator_with_wrong_length():
     with pytest.raises(ValueError):
         PrimersGenerator(length=-1, gc_percentage=.5)
+
+    with pytest.raises(ValueError):
+        PrimersGenerator(length=0, gc_percentage=.5)
