b427a1d4bcbfe02a3a1d5b7faaf240159dcd4baf
Vladimir <shitov@biocad.ru>
2021-03-09 00:02:59 +0700

    Add test_primers_generator_with_any_temperature()

--- prigen/test_prigen.py
+++ prigen/test_prigen.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 from prigen.generators import PrimersGenerator
@@ -25,3 +26,25 @@ def test_primers_generator_with_wrong_temperature_bounds():
 
     with pytest.raises(ValueError):
         PrimersGenerator(length=1, gc_percentage=.5, min_temperature=5, max_temperature=5)
+
+
+def test_primers_generator_with_wrong_gc_percentage():
+    for percent in np.linspace(-2, -0.001, 30):
+        with pytest.raises(ValueError):
+            PrimersGenerator(length=1, gc_percentage=percent)
+
+    for percent in np.linspace(1.001, 10, 30):
+        with pytest.raises(ValueError):
+            PrimersGenerator(length=1, gc_percentage=percent)
+
+
+def test_primers_generator_with_any_temperature():
+    for n_primers in range(1, 30):
+        for length in range(4, 30):
+            generator = PrimersGenerator(
+                length=length, number_of_primers=n_primers, gc_percentage=.5
+            )
+            primers = generator.generate_primers()
+
+            assert len(primers) == n_primers
+            assert all(len(primer) == length for primer in primers)
