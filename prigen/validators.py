0915f73ebc202e1f1e20ce5a04dfb90844522240
Vladimir <shitov@biocad.ru>
2021-03-08 21:40:36 +0700

    Raise error if number of primers equals 0

--- prigen/validators.py
+++ prigen/validators.py
@@ -9,7 +9,7 @@ def check_gc_percentage(gc_percentage):
 
 
 def check_number_of_primers(number_of_primers):
-    if number_of_primers < 0:
+    if number_of_primers <= 0:
         raise ValueError("Number of primers must be greater than 0")
 
 
