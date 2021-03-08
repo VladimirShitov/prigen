c31d9aa8338857543699773e817ea05230540c2e
Vladimir <shitov@biocad.ru>
2021-03-08 19:17:23 +0700

    Add PrimerGenerator.generate_primer()

--- prigen/generators.py
+++ prigen/generators.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from prigen.validators import (
     check_length,
     check_gc_percentage,
@@ -28,6 +30,50 @@ class PrimersGenerator:
         check_gc_percentage(self.gc_percentage)
         check_number_of_primers(self.number_of_primers)
         check_temperature_bounds(self.temperature_from, self.temperature_to)
+
+    @staticmethod
+    def generate_primer(length: int, gc_percentage: float) -> str:
+        """Generate a nucleotide sequence with given GC-contend and length
+
+        Parameters
+        ----------
+        length: int
+            Length of the nucleotide sequence
+        gc_percentage: float
+            GC-content of the nucleotide sequence. Must be a number between 0 and 1
+        """
+        indexes = np.arange(length)
+        np.random.shuffle(indexes)
+
+        desired_gc = int(length * gc_percentage)
+        desired_at = length - desired_gc
+
+        if desired_at:
+            desired_a = np.random.randint(desired_at + 1)
+        else:
+            desired_a = 0
+
+        if desired_gc:
+            desired_g = np.random.randint(desired_gc + 1)
+        else:
+            desired_g = 0
+
+        g_indexes = indexes[0: desired_g]
+        c_indexes = indexes[desired_g: desired_gc]
+        a_indexes = indexes[desired_gc: desired_a + desired_gc]
+
+        sequence = []
+        for i in range(length):
+            if i in g_indexes:
+                sequence.append("G")
+            elif i in c_indexes:
+                sequence.append("C")
+            elif i in a_indexes:
+                sequence.append("A")
+            else:
+                sequence.append("T")
+
+        return "".join(sequence)
 
     def generate(self):
         pass
